f1dc429c1eed02abc4baf2ca4a25eecc5536c229
q9liu <qiang.5.liu@nokia.com>
2016-12-02 17:23:27 +0000

    add files for basic data types

---
+++ Language Basics/datatypes.java
@@ -0,0 +1,18 @@
+class dataTypesDemo
+{
+
+	public static void main(String[] args)
+	{
+		
+		int  ivar = 1;
+		byte bvar = 8;
+		boolean bflag = true;
+		short sVar = 4094;
+		float e = 6.4435e-3f;
+		System.out.println("ivar:" + ivar);
+		System.out.println("bvar:" + bvar);
+		System.out.println("bflag:" + bflag);
+		System.out.println("sVar:" + sVar);
+		System.out.println("e:" + e);
+	}
+}
