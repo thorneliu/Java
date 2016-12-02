cf973426124c94182cf178585b45fcac5f4f381b
q9liu <qiang.5.liu@nokia.com>
2016-12-02 17:45:31 +0000

    add underline

--- Language Basics/datatypes.java
+++ Language Basics/datatypes.java
@@ -3,16 +3,21 @@ class dataTypesDemo
 
 	public static void main(String[] args)
 	{
-		
 		int  ivar = 1;
 		byte bvar = 8;
 		boolean bflag = true;
 		short sVar = 4094;
-		float e = 6.4435e-3f;
+		float e = 6.4435e-3f; // a float number should use suffix f
+
+		long creditCardNumber =  6225_7687_1385_7716L;
+
+
+
 		System.out.println("ivar:" + ivar);
 		System.out.println("bvar:" + bvar);
 		System.out.println("bflag:" + bflag);
 		System.out.println("sVar:" + sVar);
 		System.out.println("e:" + e);
+		System.out.println("creditCardNumber:" + creditCardNumber);
 	}
 }
